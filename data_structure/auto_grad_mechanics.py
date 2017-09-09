3cfe3d9b45e7440ed7a2408ff0d3c996977f6ea8
mi-minus <1624727417@qq.com>
2017-09-10 03:15:22 +0800

    add all

---
+++ data_structure/auto_grad_mechanics.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+import torch
+import numpy as np
+from torch.autograd import Variable
+
+
+def test1():
+    x = Variable(torch.randn(5, 5))
+    y = Variable(torch.randn(5, 5))
+    z = Variable(torch.randn(5, 5), requires_grad=True)
+    a = x + y
+    print a.requires_grad   # False
+    b = a + z
+    print b.requires_grad   # True
+
+
+
+# http://www.jianshu.com/p/cbce2dd60120
+def test_grad():
+    x = Variable(torch.ones(2), requires_grad=True)
+    z = 4 * x * x
+    y = z.norm()
+    print y
+    y.backward()
+    print x.grad
+
+
+if __name__ == '__main__':
+    test1()
+    test_grad()
